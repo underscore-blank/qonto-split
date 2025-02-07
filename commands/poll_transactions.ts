23517ce052d77bf85605dcac556315219c28def8
Guillaume Varin <guillaume.varin@ik.me>
2025-02-07 13:43:57 +0100

    ♻️ other(poll:transactions): push for review

--- commands/poll_transactions.ts
+++ commands/poll_transactions.ts
@@ -3,73 +3,101 @@ import type { CommandOptions } from '@adonisjs/core/types/ace';
 import { inject } from '@adonisjs/core';
 
 import env from '#start/env';
-import hash from '@adonisjs/core/services/hash';
 
 import QontoService from '#services/qonto_service';
 
-import { DateTime } from 'luxon';
+import { DateTime, type DateTimeUnit } from 'luxon';
 
-import { Transaction } from '#types/qonto';
+import type { Transaction } from '#types/qonto';
 
 export default class PollTransactions extends BaseCommand {
     public static commandName = 'poll:transactions';
     public static description = 'Retrieves new transactions from the Qonto API and processes them';
 
     public static options: CommandOptions = {
-        startApp: true,
-    }
+        startApp: true
+    };
 
-    private dt = DateTime.now(); // refactor
-    private transactions: Transaction[] = [];
+    @flags.string({
+        flagName: 'interval',
+        description: 'Defines the time interval for retrieving transactions',
+        alias: ['i'],
+        default: 'year'
+    })
+    declare interval: DateTimeUnit;
+
+    @flags.string({
+        flagName: 'account',
+        description: 'Specifies the bank account ID for which to retrieve transactions',
+        alias: ['a']
+    })
+    declare account: string;
+
+    private currentDateTime = DateTime.now();
 
     @inject()
     public async prepare(qontoService: QontoService) {
-        const accountsToWatchIdsEnv = env.get('QONTO_WATCH_ACCOUNTS_IDS');
-        const accountsToWatchIds = accountsToWatchIdsEnv
-            .split(',')
-            .map((id) => id.trim())
-            .filter(Boolean);
+        const watchedAccountIds = this.account
+            ? [this.account]
+            : this.getWatchedAccountIds();
 
         const transactionsByAccountIds = await Promise.all(
-            accountsToWatchIds.map(async (accountId) => {
+            watchedAccountIds.map(async (accountId) => {
                 const { transactions } = await qontoService.listTransactions({
                     bank_account_id: accountId,
 
                     'operation_type[]': 'income',
 
-                    'emitted_at_from': this.dt.startOf('month').toISO(),
-                    'emitted_at_to': this.dt.endOf('day').toISO(),
-                })
+                    emitted_at_from: this.currentDateTime.startOf(this.interval).toISO(),
+                    emitted_at_to: this.currentDateTime.endOf(this.interval).toISO()
+                });
 
                 return transactions;
             })
         );
 
-        console.log(transactionsByAccountIds.flat());
-
-        return;
-    }
-
-    public async interact() {
-        // exec list:accounts command
+        return transactionsByAccountIds.flat();
     }
 
     public async run() {
-        const Exclusion = (await import('#models/exclusion')).default;
-
-        const exclusions = await Exclusion.all();
-
-        for (const tx of this.transactions) {
-            const txIban = tx.income?.counterparty_account_number!;
-            for (const exclusion of exclusions) {
-                if (await hash.verify(exclusion.iban, txIban)) {
-                    this.logger.log(exclusion.iban);
-                }
-            }
-        }
-
-        // console.log(this.transactions);
-
         await this.terminate();
+    }
+
+    private getWatchedAccountIds(): string[] {
+        const accountsEnv = env.get('QONTO_WATCH_ACCOUNTS_IDS');
+        return accountsEnv
+            .split(',')
+            .map(id => id.trim())
+            .filter(Boolean);
+    }
+
+    // private async filterTransactions(transactions: Transaction[]) {
+    //     const Exclusion = (await import('#models/exclusion')).default;
+    //
+    //     const exclusions = await Exclusion.all();
+    //
+    //     return transactions.filter(
+    //         transaction =>
+    //     );
+    // }
+
+    @inject()
+    private async processTransaction(transaction: Transaction, qontoService: QontoService) {
+        const ProcessedTransactions = (await import('#models/processed_transaction')).default;
+
+        const vatAmount = transaction.amount * 0.2;
+        const remainingAmount = transaction.amount - vatAmount;
+
+        await qontoService.transferToVATAccount(vatAmount);
+
+        await ProcessedTransactions.create({
+            transactionId: transaction.id,
+            amount: transaction.amount,
+            reference: transaction.reference,
+            label: transaction.label,
+            amountSplit: remainingAmount
+        });
+
+        this.logger.success(`Transaction ${transaction.id} treated. VAT: ${vatAmount}`);
     }
 }
